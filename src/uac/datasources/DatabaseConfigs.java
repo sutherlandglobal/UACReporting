082f8caaa399b8164d9aaeb7311e73f455535a12
jas0ndiam0nd <jason.diamond@sutherlandglobal.com>
2014-08-31 15:26:41 -0400

    Dedicated package for pointing to database config files.

---
+++ src/uac/datasources/DatabaseConfigs.java
@@ -0,0 +1,7 @@
+package uac.datasources;
+
+public final class DatabaseConfigs 
+{
+	public final static String PRIVATE_LABEL_PROD_DB = "/opt/tomcat/apache-tomee-plus/webapps/UAC/WEB-INF/lib/conf/database/rocjfsdbs27.properties";
+	public final static String PRIVATE_LABEL_DEV_DB = "/opt/tomcat/apache-tomee-plus/webapps/UAC/WEB-INF/lib/conf/database/rocjfsdev18.properties";
+}
